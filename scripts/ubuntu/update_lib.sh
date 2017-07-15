78666775d46eca34772f1571a5c964213013f625
Memo Akten <memo@memo.tv>
2017-07-15 10:59:20 +0100

    updated lib script to support custom src path

--- scripts/ubuntu/update_lib.sh
+++ scripts/ubuntu/update_lib.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-# this script:
-# 1. copies the libtensorflow_cc.so
-#       from ofxMSATensorFlow/libs/tensorflow/lib/linux64/ to ~/lib (or user specified folder)
-# 2. adds this path to /etc/ld.so.conf.d/lib/libtensorflow_cc.so.conf 
-# 3. runs sudo ldconfig
+# this script copies the required dynamic lib (libtensorflow_cc.so) to desired folder
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "*** You need to run this script as root (try with sudo) ***"
     echo 
 	echo "This script":
 	echo " 1. copies libtensorflow_cc.so"
-	echo "      from ofxMSATensorFlow/libs/tensorflow/lib/linux64"
-	echo "      to a destination lib path (~/lib by defaut, or command line arg)"
+	echo "      from src path (ofxMSATensorFlow/libs/tensorflow/lib/linux64 by default)"
+	echo "      to destination folder (~/lib by defaut)"
 	echo " 2. adds this path to /etc/ld.so.conf.d/libtensorflow_cc.so.conf"
 	echo " 3. runs ldconfig"
 	echo
-    exit
+	echo	
+	echo "Usage (parameters optional):"
+	echo
+	echo "$ update_lib.sh [dst_folder] [src_folder]"
+	echo
+	exit
 fi
 
+# DEFAULTS
+LIB_DST=$HOME'/lib' # override with arg 1
+LIB_SRC='../../libs/tensorflow/lib/linux64/libtensorflow_cc.so' # override with arg2
 
-if [[ $# -eq 0 ]]
-then
-	LIB_DST=$HOME'/lib'
-else
-	LIB_DST=$1
-fi
-
-LIB_SRC='../../libs/tensorflow/lib/linux64/libtensorflow_cc.so'
 DST_LD_CONF='/etc/ld.so.conf.d/libtensorflow_cc.so.conf'
 
+
+if ! ([ -z "$1" ]); then LIB_DST=$1; fi
+if ! ([ -z "$2" ]); then LIB_SRC=$2; fi
+
+
 echo 'Copying' $LIB_SRC 'to' $LIB_DST
-mkdir $LIB_DST
+mkdir -p $LIB_DST
 cp $LIB_SRC $LIB_DST
 
 echo 'Writing path to' $DST_LD_CONF
